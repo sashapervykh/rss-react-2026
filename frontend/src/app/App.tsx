06ed4685a290e823fa34d0a02e42df763b7adf94
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-05-01 22:24:26 +0300

    feat: add basic button component

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,5 +1,9 @@
+import Button from "../shared/ui/Button/Button"
+
 function App() {
-  return (<>New Data Search App</>)
+  return (<>New Data Search App
+    <Button text="Click me" handleClick={() => console.log('Button clicked')} />
+  </>)
 }
 
 export default App
---
+++ frontend/src/shared/ui/Button/Button.tsx
@@ -0,0 +1,21 @@
+import { Component, type MouseEventHandler } from 'react';
+
+type ButtonProps = {
+  text: string;
+  type?: 'button' | 'submit' | 'reset';
+  handleClick: MouseEventHandler<HTMLButtonElement>;
+};
+
+class Button extends Component<ButtonProps> {
+  render() {
+    const { text, handleClick, type = 'button' } = this.props;
+
+    return (
+      <button type={type} onClick={handleClick}>
+        {text}
+      </button>
+    );
+  }
+}
+
+export default Button;
