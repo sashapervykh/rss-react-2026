e62c584f12ab1930ccb807349647235f1b71d22b
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-05-02 18:02:48 +0300

    feat: add ErrorBoundary

---
+++ frontend/src/shared/ui/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,35 @@
+import { Component, type ErrorInfo, type ReactNode } from 'react';
+
+type ErrorBoundaryProps = {
+  children: ReactNode;
+  fallback?: ReactNode;
+};
+
+type ErrorBoundaryState = {
+  hasError: boolean;
+};
+
+export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  state: ErrorBoundaryState = {
+    hasError: false,
+  };
+
+  static getDerivedStateFromError(): ErrorBoundaryState {
+    return { hasError: true, }
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        this.props.fallback ?? <div>Something went wrong.</div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
