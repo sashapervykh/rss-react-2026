e03212566e6cf9ac9156f28855ee2101976e29e9
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-05-03 14:53:37 +0300

    feat: add input styles

--- frontend/src/features/search/ui/SearchForm.tsx
+++ frontend/src/features/search/ui/SearchForm.tsx
@@ -45,12 +45,13 @@ export class SearchForm extends Component<SearchFormProps, SearchFormState> {
     const { query } = this.state;
 
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit} className='flex gap-[1rem]'>
         <input
           type="text"
           value={query}
           onChange={this.handleChange}
           placeholder="Enter movie..."
+          className='h-[100%] rounded-xl outline bg-zinc-50 p-[0.5rem]'
         />
         <Button type="submit" text="Search" />
       </form>
