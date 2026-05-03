936de7dcfd7c5e903b4f4127392096a043bce2ca
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-05-03 12:05:06 +0300

    refactor: use mocked data for development

---
+++ frontend/src/features/search/api/getMoviesList.ts
@@ -0,0 +1,27 @@
+// import { API_ROUTES } from "../../../shared/constants/apiRoutes";
+// import { API_TOKEN } from "../../../shared/constants/apiToken";
+// import { getTypedMovies } from "../lib/getTypedMovies";
+
+import { MOCKED_DATA } from "./mockedData";
+
+export async function getMoviesList(query: string) {
+    // const searchQuery = query === "" ? `${API_ROUTES.DISCOVER}?page=1` : `${API_ROUTES.SEARCH}?query=${query}&page=1`
+    // const response = await fetch(`${API_ROUTES.BASE_URL}/${searchQuery}`, {
+    //     method: "GET",
+    //     headers: {
+    //         accept: 'application/json',
+    //         Authorization: `Bearer ${API_TOKEN}`
+    //     },
+    // })
+    // if (!response.ok) {
+    //     throw new Error('Request to server failed. Try again later...')
+    // }
+
+    // const responseData: unknown = await response.json();
+    // console.log(responseData);
+    // const typedData = getTypedMovies(responseData);
+    // console.log(typedData);
+    // return typedData;
+    console.log(query)
+    return MOCKED_DATA
+}
