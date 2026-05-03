9279a596e3056e3f84d8fe49217ac4b8236ed9a7
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-05-03 17:01:15 +0300

    feat: adjust styles for logo and footer

--- frontend/src/shared/ui/Footer/Footer.tsx
+++ frontend/src/shared/ui/Footer/Footer.tsx
@@ -5,8 +5,8 @@ import { TMBD_NOTICE } from '../../constants/tmdbNotice';
 export class Footer extends Component {
   render() {
     return (
-      <footer className='flex fixed bottom-0 bg-[#F5F5F0] w-[100%] p-[2rem 0] min-h-[50px] gap-[1rem] justify-center'>
-        <div className='flex w-[200px] m-[auto_0]'>
+      <footer className='flex fixed left-0 bottom-0 bg-[#F5F5F0] w-[100vw] p-[2rem 0] min-h-[50px] gap-[1rem] justify-center'>
+        <div className='flex w-[170px] m-[auto_0]'>
           <img src={tmdbLogo} alt="API Provider Logo" />
         </div>
         <p className='flex text-sky-800 m-[auto_0] h-fit'>{TMBD_NOTICE}</p>
