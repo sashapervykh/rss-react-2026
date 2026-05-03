a73af9b0df1ecd16168128da058a9d29b8fb08d9
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-05-03 18:21:08 +0300

    refactor: rewrite MovieCard with class component

--- frontend/src/entities/movie/ui/MovieCard/MovieCard.tsx
+++ frontend/src/entities/movie/ui/MovieCard/MovieCard.tsx
@@ -1,12 +1,17 @@
+import { Component } from 'react';
 import type { MovieType } from '../../model/MovieType';
 
-export function MovieCard({ title, description }: MovieType) {
-  return (
-    <div className="p-[1.5rem] mb-[1rem] rounded-xl box-border bg-slate-50 border-sky-950 border-2">
-      <h3 className="font-bold text-left mb-[0.5rem]">Title: {title}</h3>
-      <p className="text-justify">
-        <span className="font-bold">Description:</span> {description}
-      </p>
-    </div>
-  );
+export class MovieCard extends Component<MovieType> {
+  render() {
+    const { title, description } = this.props;
+
+    return (
+      <div className="p-[1.5rem] mb-[1rem] rounded-xl box-border bg-slate-50 border-sky-950 border-2">
+        <h3 className="font-bold text-left mb-[0.5rem]">Title: {title}</h3>
+        <p className="text-justify">
+          <span className="font-bold">Description:</span> {description}
+        </p>
+      </div>
+    );
+  }
 }
