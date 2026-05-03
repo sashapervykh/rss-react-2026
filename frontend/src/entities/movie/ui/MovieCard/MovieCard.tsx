2b13ae4ab309cb0dd2a471917379b9e00859cd30
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-05-03 15:51:19 +0300

    feat: add styles to movie cards

--- frontend/src/entities/movie/ui/MovieCard/MovieCard.tsx
+++ frontend/src/entities/movie/ui/MovieCard/MovieCard.tsx
@@ -2,9 +2,9 @@ import type { MovieType } from '../../model/MovieType';
 
 export function MovieCard({ title, description }: MovieType) {
   return (
-    <div>
-      <h3>{title}</h3>
-      <p>{description}</p>
+    <div className='outline-[2px] p-[1.5rem] mb-[1rem] rounded-xl box-border bg-slate-50'>
+      <h3 className='font-bold text-left mb-[0.5rem]'>Title: {title}</h3>
+      <p className='text-justify'><span className='font-bold'>Description:</span> {description}</p>
     </div>
   );
 }
