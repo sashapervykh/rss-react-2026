b955d37a64f7f7d81606bfe764d0939384dded46
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-05-03 14:31:46 +0300

    feat: add styles to app logo

--- frontend/src/shared/ui/Header/Header.tsx
+++ frontend/src/shared/ui/Header/Header.tsx
@@ -3,8 +3,8 @@ import { Component } from 'react';
 export class Header extends Component {
   render() {
     return (
-      <header>
-        <div>KINOBASE</div>
+      <header className='flex flex-row m-[1rem_auto] justify-start w-[100%]'>
+        <div className='font-["Bebas_Neue",_sans-serif] text-4xl text-blue-900'>KINOBASE</div>
       </header>
     );
   }
