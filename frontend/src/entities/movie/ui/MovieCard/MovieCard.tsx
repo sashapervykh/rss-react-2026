44bb1689d3c10fd18ad981fd9cb102d216e16847
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-05-03 16:07:08 +0300

    feat: add footer styles

--- frontend/src/entities/movie/ui/MovieCard/MovieCard.tsx
+++ frontend/src/entities/movie/ui/MovieCard/MovieCard.tsx
@@ -2,7 +2,7 @@ import type { MovieType } from '../../model/MovieType';
 
 export function MovieCard({ title, description }: MovieType) {
   return (
-    <div className='outline-[2px] p-[1.5rem] mb-[1rem] rounded-xl box-border bg-slate-50'>
+    <div className='outline-[2px] p-[1.5rem] mb-[1rem] rounded-xl box-border bg-slate-50 ml-[2px]'>
       <h3 className='font-bold text-left mb-[0.5rem]'>Title: {title}</h3>
       <p className='text-justify'><span className='font-bold'>Description:</span> {description}</p>
     </div>
--- frontend/src/shared/ui/Footer/Footer.tsx
+++ frontend/src/shared/ui/Footer/Footer.tsx
@@ -5,12 +5,12 @@ import { TMBD_NOTICE } from '../../constants/tmdbNotice';
 export class Footer extends Component {
   render() {
     return (
-      <footer>
-        <div style={{ width: '100px' }}>
+      <footer className='flex fixed bottom-0 bg-[#F5F5F0] w-[100%] p-[2rem 0] min-h-[50px] gap-[1rem] justify-center'>
+        <div className='flex w-[200px] m-[auto_0]'>
           <img src={tmdbLogo} alt="API Provider Logo" />
         </div>
-        <span>{TMBD_NOTICE}</span>
-      </footer>
+        <p className='flex text-sky-800 m-[auto_0] h-fit'>{TMBD_NOTICE}</p>
+      </footer >
     );
   }
 }
