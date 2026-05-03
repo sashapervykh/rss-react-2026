2c0a8201e42b4cb641768d08238bc96e25960625
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-05-03 18:28:19 +0300

    feat: change app name

--- frontend/src/shared/ui/Header/Header.tsx
+++ frontend/src/shared/ui/Header/Header.tsx
@@ -5,7 +5,7 @@ export class Header extends Component {
     return (
       <header className="flex flex-row m-[0_auto_1rem_0] justify-start w-[100%]">
         <div className='font-["Bebas_Neue",_sans-serif] text-5xl text-blue-900'>
-          KINOBASE
+          CinemaCity
         </div>
       </header>
     );
